b7bb6b5cdb66fcde7d2706f4af5a8caf069bb92e
Kar111 <avetisyankarine111@gmail.com>
2022-06-02 20:24:54 +0400

    Add existing project files prior to the push to GitHub.

---
+++ src/main/java/com/medflow/controller/MedflowController.java
@@ -0,0 +1,14 @@
+package com.medflow.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping()
+public class MedflowController {
+    @GetMapping
+    public String healthCheck() {
+        return "MedFlow is up and running!!!";
+    }
+}
