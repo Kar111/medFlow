703656cf3c88ed03e415f9a1a56e483a1a5bdb31
Kar111 <avetisyankarine111@gmail.com>
2022-07-11 02:35:13 +0400

    Run some tables

--- src/main/java/com/medflow/model/Admin.java
+++ src/main/java/com/medflow/model/Admin.java
@@ -4,32 +4,32 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-//import javax.persistence.*;
-//
-//import static javax.persistence.GenerationType.SEQUENCE;
+import javax.persistence.*;
 
-//@Entity(name = "Admin")
+import static javax.persistence.GenerationType.SEQUENCE;
+
+@Entity(name = "Admin")
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 public class Admin {
 
-//    @Column(name = "firstName",
-//            nullable = false)
+    @Column(name = "firstName",
+            nullable = false)
     private String firstName;
 
-//    @Id
-//    @SequenceGenerator(
-//            name = "admin_sequence",
-//            sequenceName = "student_sequence",
-//            allocationSize = 1
-//    )
-//    @GeneratedValue(
-//            strategy = SEQUENCE,
-//            generator = "student_sequence"
-//    )
-//    @Column(name = "id",
-//            updatable = false)
+    @Id
+    @SequenceGenerator(
+            name = "admin_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = SEQUENCE,
+            generator = "student_sequence"
+    )
+    @Column(name = "id",
+            updatable = false)
     private Long id;
 
     public Admin(String firstName) {
