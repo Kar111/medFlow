982ecc25ac500e56cfe794b8a16e5240b9785748
Kar111 <avetisyankarine111@gmail.com>
2022-07-11 23:35:30 +0400

    upload the changed codes

--- src/main/java/com/medflow/MedflowApplication.java
+++ src/main/java/com/medflow/MedflowApplication.java
@@ -2,9 +2,8 @@ package com.medflow;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication(exclude = {HibernateJpaAutoConfiguration.class})
+@SpringBootApplication
 public class MedflowApplication {
 
 	public static void main(String[] args) {
